1099493e8ae3f3fb140a2fef914e726454715344
rigoriosh <rigoriosh@gmail.com>
2021-02-10 16:39:21 -0500

    add sentences

--- servidor/database/db.sql
+++ servidor/database/db.sql
@@ -1,7 +1,127 @@
+CREATE SCHEMA `programacredito` ;
+
+CREATE TABLE `programacredito`.`users` (
+  `idusers` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusers`))
+COMMENT = '	Tabla para el control de usuarios';
+
+ALTER TABLE `programacredito`.`users` 
+ADD COLUMN `email` VARCHAR(45) NOT NULL AFTER `password`;
+
+CREATE TABLE `programacredito`.`teachers` (
+  `idteachers` INT NOT NULL AUTO_INCREMENT,
+  `nombre_profesor` VARCHAR(45) NOT NULL,
+  `apellido_profesor` VARCHAR(45) NOT NULL,
+  `profesion` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idteachers`));
+
+CREATE TABLE `programacredito`.`materias` (
+  `idmaterias` INT NOT NULL AUTO_INCREMENT,
+  `nombre_materia` VARCHAR(45) NOT NULL,
+  `creditos_materia` INT NOT NULL,
+  PRIMARY KEY (`idmaterias`))
+COMMENT = '3.Existen 10 materias\n4.Cada materia equivale a 3 créditos.';
+
+CREATE TABLE `programacredito`.`roles` (
+  `idroles` INT NOT NULL AUTO_INCREMENT,
+  `nombre_rol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idroles`));
+
+CREATE TABLE `programacredito`.`clases` (
+  `idclases` INT NOT NULL AUTO_INCREMENT,
+  `nombre-clase` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idclases`));
+
+  CREATE TABLE `programacredito`.`materselected` (
+  `idmaterSelected` INT NOT NULL AUTO_INCREMENT,
+  `firstMat` VARCHAR(45) NOT NULL,
+  `secondMat` VARCHAR(45) NOT NULL,
+  `thirdMat` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idmaterSelected`));
+
+ALTER TABLE `programacredito`.`materias` 
+ADD COLUMN `idteachers` INT NOT NULL AFTER `creditos_materia`,
+ADD INDEX `idteachers_idx` (`idteachers` ASC) VISIBLE;
 
 
-CREATE TABLE `cruddatabase`.`movie_reviews` (
-  `idmovie_reviews` INT NOT NULL AUTO_INCREMENT,
-  `movie_name` VARCHAR(200) NOT NULL,
-  `movie_review` TEXT(500) NOT NULL,
-  PRIMARY KEY (`idmovie_reviews`));
+ALTER TABLE `programacredito`.`materias` 
+ADD CONSTRAINT `idteachers`
+  FOREIGN KEY (`idteachers`)
+  REFERENCES `programacredito`.`teachers` (`idteachers`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `programacredito`.`users` 
+ADD COLUMN `rol` VARCHAR(45) NULL AFTER `email`;
+
+ALTER TABLE `programacredito`.`materias` 
+DROP FOREIGN KEY `idprofesors`;
+ALTER TABLE `programacredito`.`materias` 
+CHANGE COLUMN `idprofesors` `idteachers` INT NOT NULL ,
+ADD INDEX `idteachers_idx` (`idteachers` ASC) VISIBLE,
+DROP INDEX `idprofesors_idx` ;
+;
+ALTER TABLE `programacredito`.`materias` 
+ADD CONSTRAINT `idteachers`
+  FOREIGN KEY (`idteachers`)
+  REFERENCES `programacredito`.`teachers` (`idteachers`);
+
+
+
+
+ALTER TABLE `programacredito`.`users` 
+CHANGE COLUMN `rol` `rol` INT NULL DEFAULT NULL ,
+ADD INDEX `rol_idx` (`rol` ASC) VISIBLE;
+;
+ALTER TABLE `programacredito`.`users` 
+ADD CONSTRAINT `rol`
+  FOREIGN KEY (`rol`)
+  REFERENCES `programacredito`.`roles` (`idroles`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `programacredito`.`clases` 
+CHANGE COLUMN `nombre-clase` `nombre_clase` VARCHAR(45) NOT NULL ;
+
+/////////////
+
+ALTER TABLE `programacredito`.`materselected` 
+ADD COLUMN `idusers` INT NOT NULL AFTER `thirdMat`,
+ADD INDEX `idusers_idx` (`idusers` ASC) VISIBLE;
+;
+ALTER TABLE `programacredito`.`materselected` 
+ADD CONSTRAINT `idusers`
+  FOREIGN KEY (`idusers`)
+  REFERENCES `programacredito`.`users` (`idusers`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+  ///////////////////////
+
+ ALTER TABLE `programacredito`.`users` 
+ADD COLUMN `idmaterSelected` INT NOT NULL AFTER `idclases`,
+ADD INDEX `idmaterSelected_idx` (`idmaterSelected` ASC) VISIBLE;
+;
+ALTER TABLE `programacredito`.`users` 
+ADD CONSTRAINT `idmaterSelected`
+  FOREIGN KEY (`idmaterSelected`)
+  REFERENCES `programacredito`.`materselected` (`idmaterSelected`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+  ALTER TABLE `programacredito`.`users` 
+DROP FOREIGN KEY `idmaterSelected`;
+ALTER TABLE `programacredito`.`users` 
+CHANGE COLUMN `idclases` `idclases` INT NULL ,
+CHANGE COLUMN `idmaterSelected` `idmaterSelected` INT NULL ;
+ALTER TABLE `programacredito`.`users` 
+ADD CONSTRAINT `idmaterSelected`
+  FOREIGN KEY (`idmaterSelected`)
+  REFERENCES `programacredito`.`materselected` (`idmaterSelected`);
+
+
+  //////////////////////////////
+
--- servidor/index.js
+++ servidor/index.js
@@ -1,27 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); /* for policy error of cors */
 const app = express();
 const mysql = require('mysql');
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));//for paramas that come to url
+
+/* create a conection pool to DB */
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '12345',
-    database: 'cruddatabase'
+    database: 'programacredito'
 });
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get('/api/get', (req, res)=>{
+    console.log('ingreso al servidor')
     const sqlSelectStament = 'SELECT * FROM movie_reviews';
     db.query(sqlSelectStament, (err, result) => {
-        console.log('result: ', result);
+        //console.log('result: ', result);
+        if(err) res.send(err)
+        console.log(' okresult: get');
         const miRes = JSON.stringify(result);
         res.send(miRes)
     });
+});
+
+app.get('/getProfes', (req, resp) => {
+    //console.log('req: ', req);
+    //console.log('resp: ', resp);
+    const sqlStament = 'SELECT * FROM teachers';
+    db.query(sqlStament, (err, result) => {
+        if(err) resp.send(err)
+        const miRes = JSON.stringify(result);
+        resp.send(miRes)
+    })
+});
+
+app.get('/getMaterias', (req, resp) => {
+    //console.log('req: ', req);
+    //console.log('resp: ', resp);
+    const sqlStament = 'SELECT * FROM materias';
+    db.query(sqlStament, (err, result) => {
+        if(err) resp.send(err)
+        const miRes = JSON.stringify(result);
+        resp.send(miRes)
+    })
+});
+
+app.get('/getUsers', (req, resp) => {   
+    const sqlStament = 'SELECT * FROM users';
+    db.query(sqlStament, (err, result) => {
+        if(err) resp.send(err)
+        const miRes = JSON.stringify(result);
+        resp.send(miRes)
+    })
+});
+
+app.get('/getRoles', (req, resp) => {   
+    const sqlStament = 'SELECT * FROM roles';
+    db.query(sqlStament, (err, result) => {
+        if(err) resp.send(err)
+        const miRes = JSON.stringify(result);
+        resp.send(miRes)
+    })
 });
 
 app.post("/api/insert", (req, resp) =>{
@@ -31,18 +75,99 @@ app.post("/api/insert", (req, resp) =>{
 
     const sqlInsertStament = 'INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?,?)';
     db.query(sqlInsertStament, [movie_name, movie_review], (err, result) => {
-        console.log('err:', err)
-        console.log('result: ', result);
+       
+        console.log(' okresult: post');
     }); 
 });
-/* //prueba insercion data
-app.get('/', (req, res) =>{
-    const sqlInsertStament = 'INSERT INTO movie_reviews (movie_name, movie_review) VALUES ("walsStreet", "good movie");';
-    db.query(sqlInsertStament, (err, result) => {
-        res.send("hello Rigo")
+
+app.post("/register", (req, resp)=>{
+    console.log('ingreso al servidor')
+    const username = req.body.username, password = req.body.password, email = req.body.email;
+    const stament = "INSERT INTO users (username, password, email) VALUES (?,?,?)"
+    db.query(stament, [username, password, email], (err, result) => {
+        console.log("err:", err);
+        resp.send(result);
+    })
+});
+
+app.post('/addProfessor', (req, resp) => {
+    const nombre_profesor = req.body.nombre_profesor, apellido_profesor = req.body.apellido_profesor, profesion = req.body.profesion;
+    const stament = "INSERT INTO teachers (nombre_profesor, apellido_profesor, profesion) VALUES (?,?,?)";
+    db.query(stament, [nombre_profesor, apellido_profesor, profesion], (err, result) => {
+        console.log("err:", err);
+        resp.send(result)
+    })
+});
+
+app.post('/addMaterias', (req, resp) => {
+    const nombre_materia = req.body.nombre_materia, creditos_materia = req.body.creditos_materia, idteachers = req.body.idprofesors;
+    const stament = "INSERT INTO materias (idteachers, nombre_materia, creditos_materia) VALUES (?,?,?)";
+    console.log(idteachers, nombre_materia, creditos_materia);
+    db.query(stament, [idteachers, nombre_materia, creditos_materia], (err, result) => {
+        console.log("err:", err);
+        resp.send(result)
+    })
+});
+
+app.post("/login", (req, response) => {
+    console.log('in request /login')
+    const username = req.body.username, password = req.body.password;
+    const stament = "SELECT * FROM users WHERE username = ? AND password = ?";    
+    db.query(stament, [username, password], (err, result) => {        
+        if (err) {
+            response.send({err});
+        } 
+        
+        (result.length > 0)
+        ? response.send(result)
+        : response.send({message: "Wrong username/password combination!"});
+    
+        
+    })
+});
+
+
+
+app.delete("/api/delete/:movie_name", (req, resp) => {
+    const name = req.params.movie_name;
+    const sqlStament = 'DELETE FROM movie_reviews WHERE movie_name = ?';
+    db.query(sqlStament, name, (err, result) => {
+        /* console.log('err:', err)
+        console.log('result: ', result); */
+        console.log(' okresult: delete');
     }); 
-})
- */
+});
+app.put("/api/update", (req, resp) => {
+    const name = req.body.movie_name;
+    const review = req.body.movie_review;
+    const sqlStament = 'UPDATE movie_reviews SET movie_review = ? WHERE movie_name = ?';
+    db.query(sqlStament, [review, name], (err, result) => {                
+        resp.send(result);
+    }); 
+});
+
+app.put("/updateUserRol", (req, resp) => {    
+    const idusers = req.body.idusers;
+    const idRol = req.body.idRol;
+    const sqlStament = 'UPDATE users SET rol = ? WHERE idusers = ?';
+    db.query(sqlStament, [idRol, idusers], (err, result) => {                
+        resp.send(result);
+    }); 
+});
 app.listen(3001, ()=>{
-    console.log('running o port 3001 hi');
+    console.log('hi Rigo i"m running on port 3001');
 })
+
+
+
+/* const test = ()=>{
+    
+    const stament = "INSERT INTO materSelected (firstMat, secondMat, thirdMat, idusers) VALUES (?,?,?,?)";
+    db.query(stament, ['ingles', 'fisica', 'math', 2], (err, result) => {
+        console.log("err:", err);
+        console.log(result);
+        //resp.send(result)
+    })
+}
+
+test(); */
